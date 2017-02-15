c5ada6d3ccc93e2a6e49af02d6a048374479d1a1
jaspreet57 <erjaspreet57@gmail.com>
2017-02-15 05:10:44 -0500

    angular and node wired up. next will be user management

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -9,16 +9,28 @@ function($stateProvider, $urlRouterProvider) {
     .state('home', {
       url: '/home',
       templateUrl: '/home.html',
-      controller: 'MainCtrl'
-    }).state('admin', {
+      controller: 'MainCtrl',
+      resolve: {
+        postPromise: ['posts', function(posts){
+          return posts.getAll();
+        }]
+      }
+    })
+    .state('admin', {
       url: '/admin',
       templateUrl: '/admin.html',
       controller: 'AdminCtrl'
-    }).state('posts', {
-    url: '/posts/{id}',
-    templateUrl: '/posts.html',
-    controller: 'PostsCtrl'
-  });
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl',
+      resolve: {
+        post: ['$stateParams', 'posts', function($stateParams, posts) {
+          return posts.get($stateParams.id);
+        }]
+      }
+    });
 
   $urlRouterProvider.otherwise("home");
   
@@ -26,16 +38,50 @@ function($stateProvider, $urlRouterProvider) {
 
 
 
-app.factory('posts', [function(){
+app.factory('posts', ['$http', function($http){
   var o = {
-    posts:  [
-        {title: 'post 1', upvotes: 5},
-        {title: 'post 2', upvotes: 2},
-        {title: 'post 3', upvotes: 15},
-        {title: 'post 4', upvotes: 9},
-        {title: 'post 5', upvotes: 4}
-    ]
+    posts:  []
   };
+  
+  
+  o.getAll = function() {
+    return $http.get('/posts').success(function(data){
+      angular.copy(data, o.posts);
+    });
+  };
+  
+  o.create = function(post) {
+    return $http.post('/posts', post).success(function(data){
+      o.posts.push(data);
+    });
+  };
+  
+  o.upvote = function(post) {
+    return $http.put('/posts/' + post._id + '/upvote')
+      .success(function(data){
+        post.upvotes += 1;
+      });
+  };
+  
+  
+  o.get = function(id) {
+    return $http.get('/posts/' + id).then(function(res){
+      return res.data;
+    });
+  };
+  
+  o.addComment = function(id, comment) {
+    return $http.post('/posts/' + id + '/comments', comment);
+  };
+  
+  o.upvoteComment = function(post, comment) {
+    return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote')
+      .success(function(data){
+        comment.upvotes += 1;
+      });
+  };
+  
+  
   return o;
 }]);
 
@@ -49,14 +95,9 @@ function($scope, posts){
   
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({
+      posts.create({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0,
-         comments: [
-          {author: 'Joe', body: 'Cool post!', upvotes: 0},
-          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-        ]
       });
       
       
@@ -65,7 +106,7 @@ function($scope, posts){
     };
 
     $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
+      posts.upvote(post);
     };
 }]);
 
@@ -73,23 +114,24 @@ function($scope, posts){
 
 app.controller('PostsCtrl', [
 '$scope',
-'$stateParams',
 'posts',
-function($scope, $stateParams, posts){
-  $scope.post = posts.posts[$stateParams.id];
+'post',
+function($scope, posts, post){
+  $scope.post = post;
   
   $scope.addComment = function(){
     if($scope.body === '') { return; }
-    $scope.post.comments.push({
+    posts.addComment(post._id, {
       body: $scope.body,
       author: 'user',
-      upvotes: 0
+    }).success(function(comment) {
+      $scope.post.comments.push(comment);
     });
     $scope.body = '';
   };
   
  $scope.incrementUpvotes = function(comment) {
-    comment.upvotes += 1;
+    posts.upvoteComment(post, comment);
  };
   
 }]);
